19d5536c9febdd81f405040101e41cc2a9158c63
Nikolai <ya.pikus@gmail.com>
2022-02-04 04:55:46 +0300

    Security update

--- Project/Main.py
+++ Project/Main.py
@@ -1,19 +1,14 @@
 import asyncio
 import aiogram
+from TOKEN import TOKEN
 
 
 class Bot(object):
     def __init__(self):
-        self.__TOKEN = ''
+        self.__TOKEN = TOKEN
         self.__name = 'Олег'
         self.__login = r'@nps_rf_bot'
         self.__picture = True
 
     def __str__(self):
         return f'Name: {self.__name} \nlogin: {self.__login} \nHas picture? -> {self.__picture}'
-
-
-
-
-
-
