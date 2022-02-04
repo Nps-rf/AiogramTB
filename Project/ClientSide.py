e5e03da5565c64681476c6e0ea5b630a446305be
Nikolai <ya.pikus@gmail.com>
2022-02-04 16:43:46 +0300

    Buttons update

--- Project/ClientSide.py
+++ Project/ClientSide.py
@@ -1,8 +1,10 @@
 from aiogram import types, Dispatcher
+from Maintenance.Buttons import Buttons
 
 
 async def start_message(message: types.Message):
-    await message.answer('Добро пожаловать!')
+    await message.answer(text='Добро пожаловать!', reply_markup=Buttons)
+    await message.delete()
 
 
 def activate_message_handlers(dp: Dispatcher):
